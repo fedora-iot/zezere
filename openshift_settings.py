52b1f80b684ba456849459ba2925b33ce43831c6
Patrick Uiterwijk <patrick@puiterwijk.org>
2019-10-10 07:50:20 +0000

    Add openshift settings
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

---
+++ openshift_settings.py
@@ -0,0 +1,14 @@
+import os
+
+from zezere.settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ['DB_NAME'],
+        'USER': os.environ['DB_USER'],
+        'PASSWORD': os.environ['DB_PASS'],
+        'HOST': os.environ['DB_HOST'],
+        'PORT': os.environ.get('DB_PORT', ''),
+    }
+}
