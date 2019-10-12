b42099067e97173d803595f981c5c0d830b3ea5e
Patrick Uiterwijk <patrick@puiterwijk.org>
2019-10-12 21:53:13 +0200

    Remove unneeded imports
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- zezere/views_portal.py
+++ zezere/views_portal.py
@@ -1,6 +1,4 @@
-from django.contrib.auth.models import User, Group
 from django.db import transaction
-from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
