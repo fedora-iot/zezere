1721118375beae49c3a1a30d81c643cbb62a6c35
Patrick Uiterwijk <patrick@puiterwijk.org>
2019-10-12 21:52:50 +0200

    Implement claiming
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- zezere/views_portal.py
+++ zezere/views_portal.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User, Group
+from django.db import transaction
 from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
 from zezere.models import Device
@@ -17,7 +18,15 @@ def index(request):
 @login_required
 def claim(request):
     if request.method == "POST":
-        return HttpResponse("POST response")
+        with transaction.atomic():
+            device = get_object_or_404(
+                Device,
+                mac_address=request.POST['mac_address'],
+                owner__isnull=True,
+            )
+            device.owner = request.user
+            device.save()
+        return redirect("/portal/claim/")
 
     context = {
         "unclaimed_devices": Device.objects.filter(owner__isnull=True),
