6d04d7306dd4b1708e70890b1aa54d46693a9463
Patrick Uiterwijk <patrick@puiterwijk.org>
2019-10-13 20:57:57 +0200

    Name all views
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- zezere/urls.py
+++ zezere/urls.py
@@ -14,18 +14,22 @@ urlpatterns = [
     path(
         '',
         views.index,
+        name='index',
     ),
     path(
         'ping/',
         views.ping,
+        name='ping',
     ),
     path(
         'admin/',
         admin.site.urls,
+        name='admin',
     ),
     path(
         'accounts/',
         include('django.contrib.auth.urls'),
+        name='accounts',
     ),
 
     # Portal
@@ -37,16 +41,19 @@ urlpatterns = [
     path(
         'portal/',
         views_portal.index,
+        name='portal_index',
     ),
     path(
         'portal/claim/',
         views_portal.claim,
+        name='portal_claim',
     ),
 
     # API
     path(
         'api/',
         include(router.urls),
+        name='apis',
     ),
     path(
         'api-auth/',
@@ -54,39 +61,48 @@ urlpatterns = [
             'rest_framework.urls',
             namespace='rest_framework',
         ),
+        name='authed_apis',
     ),
 
     # Netboot
     path(
         'netboot/x64',
         views_netboot.efi_static_server("shimx64.efi"),
+        name='netboot_shim',
     ),
     path(
         'netboot/grubx64.efi',
         views_netboot.efi_static_server('grubx64.efi'),
+        name='netboot_grub',
     ),
     path(
         'netboot//grubx64.efi',
         views_netboot.efi_static_server('grubx64.efi'),
+        name='netboot_grub_double_slash',
     ),
     path(
         'netboot/grub.cfg',
         views_netboot.static_grub_cfg,
+        name='netboot_grubcfg_static',
     ),
     path(
         'netboot/grubcfg/<str:mac_addr>',
         views_netboot.dynamic_grub_cfg,
+        name='netboot_grubcfg_dynamic',
     ),
     path(
         'netboot/kickstart/<str:mac_addr>',
         views_netboot.kickstart,
+        name='netboot_kickstart',
     ),
     path(
         'netboot/ignition/<str:mac_addr>',
         views_netboot.ignition_cfg,
+        name='netboot_ignition_cfg',
     ),
     path(
         'netboot/proxydl/<str:mac_addr>/<str:filetype>',
         views_netboot.static_proxy,
+        name='netboot_proxydl',
     ),
 ]
