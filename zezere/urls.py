fdeea7074dafb002cf9eef989f9c9937aba895ea
Patrick Uiterwijk <patrick@puiterwijk.org>
2019-10-13 20:55:14 +0200

    Re-indent urls
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- zezere/urls.py
+++ zezere/urls.py
@@ -11,27 +11,82 @@ router = routers.DefaultRouter()
 router.register(r'devices/unowned', views.UnownedDevicesViewSet)
 
 urlpatterns = [
-    path('', views.index),
-    path('ping/', views.ping),
-    path('admin/', admin.site.urls),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path(
+        '',
+        views.index,
+    ),
+    path(
+        'ping/',
+        views.ping,
+    ),
+    path(
+        'admin/',
+        admin.site.urls,
+    ),
+    path(
+        'accounts/',
+        include('django.contrib.auth.urls'),
+    ),
 
     # Portal
-    path('accounts/signup/', views.SignUp.as_view(), name='signup'),
-    path('portal/', views_portal.index),
-    path('portal/claim/', views_portal.claim),
+    path(
+        'accounts/signup/',
+        views.SignUp.as_view(),
+        name='signup',
+    ),
+    path(
+        'portal/',
+        views_portal.index,
+    ),
+    path(
+        'portal/claim/',
+        views_portal.claim,
+    ),
 
     # API
-    path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path(
+        'api/',
+        include(router.urls),
+    ),
+    path(
+        'api-auth/',
+        include(
+            'rest_framework.urls',
+            namespace='rest_framework',
+        ),
+    ),
 
     # Netboot
-    path('netboot/x64', views_netboot.efi_static_server("shimx64.efi")),
-    path('netboot/grubx64.efi', views_netboot.efi_static_server('grubx64.efi')),
-    path('netboot//grubx64.efi', views_netboot.efi_static_server('grubx64.efi')),
-    path('netboot/grub.cfg', views_netboot.static_grub_cfg),
-    path('netboot/grubcfg/<str:mac_addr>', views_netboot.dynamic_grub_cfg),
-    path('netboot/kickstart/<str:mac_addr>', views_netboot.kickstart),
-    path('netboot/ignition/<str:mac_addr>', views_netboot.ignition_cfg),
-    path('netboot/proxydl/<str:mac_addr>/<str:filetype>', views_netboot.static_proxy),
+    path(
+        'netboot/x64',
+        views_netboot.efi_static_server("shimx64.efi"),
+    ),
+    path(
+        'netboot/grubx64.efi',
+        views_netboot.efi_static_server('grubx64.efi'),
+    ),
+    path(
+        'netboot//grubx64.efi',
+        views_netboot.efi_static_server('grubx64.efi'),
+    ),
+    path(
+        'netboot/grub.cfg',
+        views_netboot.static_grub_cfg,
+    ),
+    path(
+        'netboot/grubcfg/<str:mac_addr>',
+        views_netboot.dynamic_grub_cfg,
+    ),
+    path(
+        'netboot/kickstart/<str:mac_addr>',
+        views_netboot.kickstart,
+    ),
+    path(
+        'netboot/ignition/<str:mac_addr>',
+        views_netboot.ignition_cfg,
+    ),
+    path(
+        'netboot/proxydl/<str:mac_addr>/<str:filetype>',
+        views_netboot.static_proxy,
+    ),
 ]
