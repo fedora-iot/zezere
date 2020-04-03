0781869aebe7c8e5764e3f983efb05ca9bef6526
Patrick Uiterwijk <patrick@puiterwijk.org>
2020-04-03 08:49:05 +0200

    Enable collectstatic for openshift
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- manage.py
+++ manage.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python
 import zezere
 
+from sys import argv
+from os import environ
+
 if __name__ == "__main__":
+    if argv[1] == "collectstatic":
+        # For collectstatic, just set some auth method and key to make it not crash
+        environ["AUTH_METHOD"] = "local"
+        environ["SECRET_KEY"] = "collectstatic"
     zezere.run_django_management()
