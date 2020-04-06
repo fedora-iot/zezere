bf69fd2265a0f3fda9b5de76682ba10c0606b74b
Patrick Uiterwijk <patrick@puiterwijk.org>
2020-04-06 14:28:53 +0200

    Add logs to mod_wsgi-express
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- app.sh
+++ app.sh
@@ -5,6 +5,9 @@ ARGS=""
 
 ARGS="$ARGS --log-to-terminal"
 ARGS="$ARGS --port 8080"
+ARGS="$ARGS --enable-sendfile"
+ARGS="$ARGS --startup-log"
+ARGS="$ARGS --access-log"
 ARGS="$ARGS --url-alias /static static"
 # Also make sure the static files are available under netboot
 ARGS="$ARGS --url-alias /netboot/aarch64/static static"
