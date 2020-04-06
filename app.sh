263866e7c5d7c1d704c62d3c8e00db5dd53a7cef
Patrick Uiterwijk <patrick@puiterwijk.org>
2020-04-06 16:19:21 +0200

    Make static files available for netboot/debug
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- app.sh
+++ app.sh
@@ -12,5 +12,7 @@ ARGS="$ARGS --url-alias /static static"
 # Also make sure the static files are available under netboot
 ARGS="$ARGS --url-alias /netboot/aarch64/static static"
 ARGS="$ARGS --url-alias /netboot/x86_64/static static"
+ARGS="$ARGS --url-alias /netboot/debug/aarch64/static static"
+ARGS="$ARGS --url-alias /netboot/debug/x86_64/static static"
 
 exec mod_wsgi-express start-server $ARGS wsgi.py
